417551a2a8c044d31f584a72b6edeeed15e89664
aditya270520 <75534658+aditya270520@users.noreply.github.com>
2020-12-20 13:54:19 +0530

    Add files via upload

---
+++ to_find_largest_number_among_three_number.py
@@ -0,0 +1,11 @@
+num1 = 1000
+num2 = 1454
+num3 = 1256
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
