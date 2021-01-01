49a7f0588564cba28d7a4af560d72d4f64b411e9
aditya270520 <75534658+aditya270520@users.noreply.github.com>
2021-01-01 10:40:23 +0530

    Add files via upload

---
+++ couple_maker.py
@@ -0,0 +1,19 @@
+a = "virat"
+b = 'anushka'
+c = a[0:3] + b[2:8]
+
+d ="saif"
+e = "kareena"
+f = d[0:5] + e[3:7]
+
+g = "parth"
+h = "gazal"
+i = g[0:2] + h[2:5]
+
+j = "sankalp"
+k = "priya"
+l = j[0:2] + k[1:5]
+print(c)
+print(f)
+print(i)
+print(l)
